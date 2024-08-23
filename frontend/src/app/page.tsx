4c54edda08d491bc2749e4b382cba0767ce8399e
DavidSparker <davidsparker0417@gmain.com>
2024-08-23 17:36:07 +0900

    - switch between main/analyze table

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,62 +6,149 @@ import "./style.css";
 
 export default function Home() {
   const [transactions, setTransactions] = useState([])
+  const [summary, setSummary] = useState([])
   const [fetching, setFetching] = useState(false)
+  const [mode, setMode] = useState('main')
+
+  var num_per_page = 100;
+  var navs_arr     = [];
 
   async function fetchTransactions() {
     setFetching(true)
+    setMode('main')
     const resp = await axios.get(`https://dev.sonexdigital.com/backend/api/raydium`)
     // console.log(`[DAVID] (fetchTransactions) resp =`, resp)
     setTransactions(resp.data.data)
     setFetching(false)
+
+    document.getElementById("tbl_analyse").style.display = "none";
+    document.getElementById("tbl_transaction").style.display = "inline-grid";
+    document.getElementById("div_time_range").style.display = "block";
   }
+
+  async function fetchAnalyze() {
+    setFetching(true)
+    setMode('anal')
+    const resp = await axios.get(`https://dev.sonexdigital.com/backend/api/raydium/summary`)
+
+    console.log(`[DAVID] (fetchTransactions) resp =`, resp)
+
+    var num_navs  = Math.ceil(resp.data.data.length / num_per_page);
+    var disp_data = resp.data.data.splice(0, num_per_page);
+
+    for(var i = 0; i < num_navs; i ++)
+    {
+      navs_arr.push(i);
+    }
+
+    setSummary(disp_data)
+
+    setFetching(false)
+
+    document.getElementById("tbl_analyse").style.display = "inline-grid";
+    document.getElementById("tbl_transaction").style.display = "none";
+    document.getElementById("div_time_range").style.display = "none";
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div id="action_area">
-        <label>Date Range: </label>
-        <input type="calendar" id="txt_start"></input>
-        <label> - </label>
-        <input type="calendar" id="txt_end"></input>
-        <input type="button" id="btn_get_result" onClick={() => fetchTransactions()} value="Get Result"></input>
-        <input type="button" id="btn_view_analyse" value="Analyse"></input>
+        <div id="div_time_range">
+          <label>Date Range: </label>
+          <input type="calendar" id="txt_start"></input>
+          <label> - </label>
+          <input type="calendar" id="txt_end"></input>
+          <input type="button" id="btn_get_result" onClick={() => fetchTransactions()} value="Get Result"></input>
+        </div>
+        <input type="button" id="btn_view_analyse" onClick={() => fetchAnalyze()} value="Analyse"></input>
       </div>
-      {
-        <table className="main_table">
-            <thead>
-              <tr>
-                <th>No</th>
-                <th>Time</th>
-                <th>Wallet ID</th>
-                <th>Plateform</th>
-                <th>Token</th>
-                <th>Action</th>
-                <th>Spent</th>
-                <th>Receive</th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                fetching
-                ? <h1>Fetching Data ...</h1>
-                : 
-                transactions.map((t: any, i: number) => 
-                {
-                  return (<tr>
-                    <td>{i}</td>
-                    <td>{t.when}</td>
-                    <td>{t.who}</td>
-                    <td>{t.where}</td>
-                    <td>{t.what}</td>
-                    <td class={t.how}>{t.how}</td>
-                    <td>{t.sentAsset.amount}</td>
-                    <td>{t.rcvAsset.amount}</td>
-                  </tr>
-                  )
-                })
-              }
-            </tbody>
-          </table>
-      }
+
+      <table className="main_table" id="tbl_analyse">
+        <thead>
+          <tr>
+            <th width="3%">No</th>
+            <th width="20%">Wallet</th>
+            <th width="10%">Num Trades</th>
+            <th width="7%">Num Tokens</th>
+            <th width="9%">Total Spent</th>
+            <th width="9%">Total Receive</th>
+            <th width="10%">Profit</th>
+            <th width="5%">Win Rate</th>
+            <th width="7%">ROI</th>
+            <th width="6%">Trd / Wallet</th>
+            <th width="10%">Last Trade Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            fetching
+              ? <h1>Fetching Data ...</h1>
+              : summary.map((t: any, i: number) => {
+                return (<tr>
+                  <td width="3%">{i + 1}</td>
+                  <td width="20%">{t.wallet}</td>
+                  <td width="10%">{t.numTrades}</td>
+                  <td width="7%">{t.numTokens}</td>
+                  <td width="9%">{t.totalSpent}</td>
+                  <td width="9%">{t.totalReceive}</td>
+                  <td width="10%">{t.profit}</td>
+                  <td width="5%">{t.winRate}</td>
+                  <td width="7%">{t.roi}</td>
+                  <td width="6%">{t.tradesPerToken}</td>
+                  <td width="10%">{t.lastTradeTime}</td>
+                </tr>
+                )
+              })
+          }
+
+          <tr class="navigation">
+            <td colspan="11">
+              <dl>
+                <dd>1</dd>
+                <dd>2</dd>
+                <dd>3</dd>
+                <dd>4</dd>
+                <dd>5</dd>
+                <dd>6</dd>
+              </dl>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="main_table" id="tbl_transaction">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Time</th>
+            <th>Wallet ID</th>
+            <th>Plateform</th>
+            <th>Token</th>
+            <th>Action</th>
+            <th>Spent</th>
+            <th>Receive</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            fetching
+              ? <h1>Fetching Data ...</h1>
+              :
+              transactions.map((t: any, i: number) => {
+                return (<tr>
+                  <td>{i}</td>
+                  <td>{t.when}</td>
+                  <td>{t.who}</td>
+                  <td>{t.where}</td>
+                  <td>{t.what}</td>
+                  <td class={t.how}>{t.how}</td>
+                  <td>{t.sentAsset.amount}</td>
+                  <td>{t.rcvAsset.amount}</td>
+                </tr>
+                )
+              })
+          }
+        </tbody>
+      </table>
     </main>
   )
 }
